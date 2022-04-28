1f013f4b713a19d19faadfc02a433518a1ad6825
andy-dextrous <marketing@andrewscrivens.com.au>
2022-04-29 08:10:22 +1000

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,18 @@
-import React, { useState } from "react"
+import React, { useLayoutEffect, useState } from "react"
 import Nav from "./nav/Nav"
 import Footer from "./footer/Footer"
 import SmoothWrapper from "./SmoothWrapper"
 import useStopScroll from "../hooks/useStopScroll"
+import { ScrollTrigger } from "../gsap"
 
 export const MenuContext = React.createContext()
 
 export function Layout({ children, smoothScroll = true }) {
   // App-level state
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  useLayoutEffect(() => {
+    ScrollTrigger.refresh()
+  }, [])
   useStopScroll(isMenuOpen)
 
   return (
