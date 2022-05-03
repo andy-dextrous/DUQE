1450e9621f714d5ac9334d7b8f92eec767bd0568
Andrew Scrivens <andrew.scrivens@hotmail.com>
2022-05-03 14:03:48 +1000

    update stopScroll hook

--- src/components/nav/Hamburger.js
+++ src/components/nav/Hamburger.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react"
 import { MenuContext } from "../Layout"
-import { SmoothContext } from "../SmoothWrapper"
+
 import useContrastingColor from "../../hooks/useContrastingColor"
 
 import ToggleIcon from "../../assets/icons/MenuToggleIcon"
@@ -9,15 +9,10 @@ import { Button, Flex, Icon } from "@chakra-ui/react"
 
 function Hamburger() {
   const { setIsMenuOpen, isMenuOpen } = useContext(MenuContext)
-  const smootherInstance = useContext(SmoothContext)
   const ref = React.useRef()
   const [color, setColor] = useState("white")
 
   useContrastingColor({ color, setColor }, ref, { fill: color })
-  useEffect(() => {
-    if (!smootherInstance) return
-    smootherInstance.paused(isMenuOpen ? true : false)
-  }, [isMenuOpen, smootherInstance])
 
   return (
     <Flex zIndex="popover" h="176px" ref={ref}>
--- src/hooks/useStopScroll.js
+++ src/hooks/useStopScroll.js
@@ -1,6 +1,14 @@
-import { useEffect } from "react"
+import { useEffect, useContext } from "react"
+import { SmoothContext } from "../components//SmoothWrapper"
 
 export default function useStopScroll(bool) {
+  const smootherInstance = useContext(SmoothContext)
+
+  useEffect(() => {
+    if (!smootherInstance) return
+    smootherInstance.paused(bool ? true : false)
+  }, [bool, smootherInstance])
+
   useEffect(() => {
     const body = document.querySelector("body")
     bool
