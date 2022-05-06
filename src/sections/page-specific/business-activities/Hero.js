d396b8402af5729d8d67ddb1929296c88b189293
andy-dextrous <marketing@andrewscrivens.com.au>
2022-05-06 22:19:01 +1000

    Update Hero.js

--- src/sections/page-specific/business-activities/Hero.js
+++ src/sections/page-specific/business-activities/Hero.js
@@ -1,34 +1,69 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
+import { gsap, ScrollTrigger } from "../../../gsap"
+
 import SectionWrapper from "../../../components/SectionWrapper"
-import { Heading, Image, Stack, Text, VStack } from "@chakra-ui/react"
 import ScrollDown from "../../../assets/icons/ScrollDown"
+import { Heading, Image, Stack, Text, VStack } from "@chakra-ui/react"
+import YellowHeading from "../../../components/common/YellowHeading"
 
 function Hero() {
+  const img = useRef()
+  const img2 = useRef()
+  const animation = useRef()
+
+  useEffect(() => {
+    gsap.from(img.current, { opacity: 0, duration: 0.3, ease: "Power3.in" })
+
+    if (!ScrollTrigger || animation.current) return
+    const tl = gsap.timeline({
+      paused: true,
+      scrollTrigger: {
+        trigger: img.current,
+        toggleActions: "play none reverse none",
+        start: "top top",
+        end: "bottom top",
+        scrub: 0.5,
+      },
+    })
+    tl.to(img.current, {
+      scale: 1.4,
+      ease: "linear",
+    })
+    tl.to(
+      img2.current,
+      {
+        rotate: 10,
+        x: 100,
+        y: 120,
+      },
+      0
+    )
+  }, [])
+
   return (
     <SectionWrapper
-      bgImage="https://res.cloudinary.com/andrew-scrivens-guitar-lessons/image/upload/q_auto/v1651121618/DUQE/Business_activities.png"
-      h="100vh"
       isFirstSection
       overlay
+      bgImage="https://res.cloudinary.com/andrew-scrivens-guitar-lessons/image/upload/q_auto/v1651121618/DUQE/Business_activities.png"
+      h="100vh"
       zIndex="1"
       overlayStyle={{
         bg: "linear-gradient(179.45deg, rgba(11, 11, 11, 0) 32.43%, #0B0B0B 99.75%),radial-gradient(84.77% 84.77% at 73.88% 44.21%, rgba(11, 11, 11, 0) 0%, #0B0B0B 100%)",
       }}
-      containerSize="xl"
+      ref={img}
     >
-      <Stack h="full" w="full" align="center" direction="row">
+      <Stack layerStyle="fillSpace" align="center" direction="row">
         <VStack
           spacing={[8, 8, 12]}
           alignItems="start"
-          w="full"
           width={["full", "full", "50%"]}
         >
           <Heading as="h1" color="white">
             Business
-            <Text as="span" color="brandYellow.default">
+            <YellowHeading>
               <br />
               Activities
-            </Text>
+            </YellowHeading>
           </Heading>
           <Heading as="h3" color="white" className="thin-h3">
             Setting up in DUQE means freedom. Freedom to chase your dreams.
@@ -43,6 +78,7 @@ function Hero() {
         top="50px"
         right="0"
         opacity="50%"
+        ref={img2}
       />
       <ScrollDown />
     </SectionWrapper>
