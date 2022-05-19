2efd0c156da531ff9dde7c1f7d8c46e47498d88b
andy-dextrous <marketing@andrewscrivens.com.au>
2022-05-19 21:16:27 +1000

    Update createRedirects.js

--- src/utils/createRedirects.js
+++ src/utils/createRedirects.js
@@ -1,11 +1,12 @@
 module.exports = async ({ actions }, redirects) => {
+  if (!redirects) return
+  const { createRedirect } = actions
+
   createRedirect({
     fromPath: `/license-application-form`,
     toPath: `https://www.tfaforms.com/4978118`,
   })
 
-  if (!redirects) return
-  const { createRedirect } = actions
   // Create redirects from all 301s stored inside the Wordpress Redirection plugin
   redirects.length &&
     redirects.forEach(redirect => {
