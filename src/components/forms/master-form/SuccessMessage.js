ac86e23d04d805051a471acb9e96a2b96ba81ccc
Andrew Scrivens <andrew.scrivens@hotmail.com>
2022-05-25 20:09:34 +1000

    match Hubspot field names

--- src/components/forms/master-form/SuccessMessage.js
+++ src/components/forms/master-form/SuccessMessage.js
@@ -3,7 +3,7 @@ import React from "react"
 
 function SuccessMessage({ formHeight, ...props }) {
   return (
-    <Box height={formHeight} {...props}>
+    <Box flex="1" height={formHeight} {...props}>
       <Heading>Success</Heading>
       <Text>Thank you for your enquiry, we'll be in touch soon.</Text>
     </Box>
